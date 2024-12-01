928ff70df984f1f9217ccd2ac3b7a83eba034bbd
gameswu <whx24@mails.tsinghua.edu.cn>
2024-12-01 13:46:17 +0800

    Site updated: 2024-12-01 13:46:17

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732972379466";
+const VERSION = "1733031819996";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
